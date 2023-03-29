337c1296f0f49f619532cbc814aa49fa3c57633a
Oumaima Nejjari <oumanj1998@gmail.com>
2023-03-29 22:48:25 +0000

    Implementing required changes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,4 @@
 import './style.css';
-import displayTasks from './modules/Structure.js';
-import addTask from './modules/Add.js';
-import getFromStorage from './modules/Storage.js';
 
 const allTasks = [
   {
@@ -32,37 +29,13 @@ if (allTasks.length === 0) {
   tasksListDisplay.innerHTML = '<hr/><p>No tasks available now!</p>';
 }
 
-/* Creating footer */
-const footer = () => {
-  const todoListContainer = document.querySelector('.todo-list');
-  const display = document.querySelector('.display-list');
+const tasks = allTasks.map((todo) => ` 
+  <div class="tasks">
+    <div class="task"><input type="checkbox" id="demoCheckbox" name="checkbox" value="1">
+      <label for="demoCheckbox">${todo.description}</label>
+    </div>
+    <i class="fa fa-ellipsis-v" aria-hidden="true"></i>
+  </div>
+  `).join('');
 
-  const lowDiv = document.createElement('div');
-  lowDiv.classList.add('footer');
-  todoListContainer.appendChild(lowDiv);
-  lowDiv.innerHTML = `
-      <button id='clear-tasks'>Clear all completed.</button>
-    `;
-
-  const clearTasks = document.getElementById('clear-tasks');
-  clearTasks.addEventListener('click', () => {
-    let allTasks = JSON.parse(getFromStorage('todo')) || [];
-    allTasks = allTasks.filter((e) => e.completed !== true);
-    localStorage.setItem('todo', JSON.stringify(allTasks));
-    display.innerHTML = '';
-    JSON.parse(getFromStorage('todo'));
-    allTasks.forEach((element) => {
-      displayTasks(element);
-    });
-  });
-};
-
-/* Adding a new task */
-addTask();
-
-window.addEventListener('DOMContentLoaded', () => {
-  allTasks.forEach((i) => {
-    displayTasks(i);
-  });
-  footer();
-});
+tasksListDisplay.innerHTML = tasks;
