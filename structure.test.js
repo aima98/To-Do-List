a5c71b100ef6d8ddabe897296d10560873ef2e5e
Tom Daniel <tomudoh258@gmail.com>
2023-04-05 22:17:45 +0100

    Test function forremoving task

--- structure.test.js
+++ structure.test.js
@@ -17,3 +17,55 @@ describe('add list item', () => {
     expect(listContainer).toHaveLength(1);
   });
 });
+
+
+describe("removeTask", () => {
+  let taskList;
+
+  beforeEach(() => {
+    taskList = [
+      { index: 1, description: "Task 1" },
+      { index: 2, description: "Task 2" },
+      { index: 3, description: "Task 3" },
+    ];
+  });
+
+  it("should remove the task with the given index from the task list", () => {
+    const app = new App();
+    app.taskList = taskList;
+    const indexToRemove = 2;
+    app.removeTask(indexToRemove);
+    expect(app.taskList).toEqual([
+      { index: 1, description: "Task 1" },
+      { index: 2, description: "Task 3" },
+    ]);
+  });
+
+  it("should update the index of the remaining tasks", () => {
+    const app = new App();
+    app.taskList = taskList;
+    const indexToRemove = 2;
+    app.removeTask(indexToRemove);
+    expect(app.taskList[0].index).toBe(1);
+    expect(app.taskList[1].index).toBe(2);
+  });
+
+  it("should sort the tasks by index after removing one", () => {
+    const app = new App();
+    app.taskList = taskList;
+    const indexToRemove = 2;
+    app.removeTask(indexToRemove);
+    expect(app.taskList).toEqual([
+      { index: 1, description: "Task 1" },
+      { index: 2, description: "Task 3" },
+    ]);
+  });
+
+  it("should update the storage and display the list after removing a task", () => {
+    const app = new App();
+    app.taskList = taskList;
+    app.removeTask(2);
+    expect(app.updateStorage).toHaveBeenCalled();
+    expect(app.displayList).toHaveBeenCalled();
+  });
+});
