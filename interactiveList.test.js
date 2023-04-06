bba090e689c2807220bce624ae85cac44e819601
Oumaima Nejjari <oumanj1998@gmail.com>
2023-04-06 19:53:06 +0000

    Create test function

---
+++ interactiveList.test.js
@@ -0,0 +1,13 @@
+/**
+ * @jest-environment jsdom
+ */
+import InteractiveList from './src/modules/InteractiveList.js';
+
+describe('clear completed tasks', () => {
+  const task = new InteractiveList();
+
+  
+
+
+})
+
